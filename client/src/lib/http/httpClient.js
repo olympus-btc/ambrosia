504718364de430392223e3fe9ed0c22047094ca1
Vidarte-Alberto <a19100306@ceti.mx>
2026-02-09 13:57:53 -0600

    refactor(client): rename apiClient to httpClient and componentize in httpClient.js and httpWrapper.js

---
+++ client/src/lib/http/httpClient.js
@@ -0,0 +1,30 @@
+import { httpWrapper } from "./httpWrapper";
+export async function httpClient(endpoint, options = {}) {
+  const { skipRefresh = false, ...httpOptions } = options;
+
+  const AUTH_EXCLUDED_PATHS = ["/auth", "/wallet"];
+
+  const shouldRefreshToken = (status, endpoint, skipRefresh) => {
+    if (status !== 401 || skipRefresh) return false;
+    return !AUTH_EXCLUDED_PATHS.some((path) => endpoint.startsWith(path));
+  };
+
+  const response = await httpWrapper(endpoint, httpOptions);
+
+  if (shouldRefreshToken(response.status, endpoint, skipRefresh)) {
+    const refreshResponse = await httpWrapper("/auth/refresh", {
+      method: "POST",
+    });
+
+    if (refreshResponse.status === 401) {
+      window.dispatchEvent(new Event("auth:expired"));
+    }
+    return await httpWrapper(endpoint, httpOptions);
+  }
+
+  if (response.status === 401) window.dispatchEvent(new Event("auth:expired"));
+  if (response.status === 403)
+    window.dispatchEvent(new Event("auth:forbidden"));
+
+  return response;
+}
