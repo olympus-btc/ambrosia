c1895bc874923ac37182dcdb51dff2941cf7b6ef
Ivan Robles <irae45@gmail.com>
2026-05-11 16:55:01 -0600

    add products and categories locales files.

---
+++ client/src/components/pages/Store/Products/Categories/locales/es.js
@@ -0,0 +1,35 @@
+const categoriesEs = {
+  categories: {
+    title: "Categorías",
+    subtitle: "Gestiona las categorías de tus productos",
+    addCategory: "Agregar Categoría",
+    tableAriaLabel: "Tabla de categorías",
+    edit: "Editar",
+    delete: "Eliminar",
+    name: "Nombre",
+    type: "Tipo",
+    actions: "Acciones",
+    modal: {
+      titleAdd: "Agregar Categoría",
+      titleEdit: "Editar Categoría",
+      titleDelete: "Eliminar Categoría",
+      subtitleDelete: "Estás seguro que deseas eliminar",
+      warningDelete: "Esta acción no se puede deshacer.",
+      categoryNameLabel: "Nombre de la Categoría",
+      categoryNamePlaceholder: "Electrónica",
+      categoryTypeLabel: "Tipo",
+      categoryTypePlaceholder: "product",
+      submitButton: "Agregar",
+      editButton: "Guardar",
+      cancelButton: "Cancelar",
+      deleteButton: "Eliminar",
+      errorMsgInputFieldEmpty: "Por favor, complete este campo.",
+    },
+    toasts: {
+      genericErrorTitle: "Error",
+      genericErrorDescription: "No se pudo completar la operación.",
+    },
+  },
+};
+
+export default categoriesEs;
